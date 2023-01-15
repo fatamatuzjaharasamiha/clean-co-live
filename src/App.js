5205818abbf24ddaeb184b1f2b3d639df1edf7e4
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2023-01-16 01:33:20 +0600

    Home page Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home/Home';
+import Login from './pages/Login';
+import Services from './pages/Services';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    AOS.init();
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/services' element={<Services></Services>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
+      </Navbar>
+
     </div>
   );
 }
---
+++ src/pages/Home/Landing.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import bucketGirl from '../../assets/image/bucketgirl.png'
+const Landing = () => {
+    return (
+        <>
+            <div class="hero h-screen lg:h-[60vh] bg-orange-100 mt-16">
+                <div class="hero-content flex-col lg:flex-row">
+
+                    <div data-aos="fade-right">
+                        <h1  class="text-5xl font-bold">Professional Cleaning Service</h1>
+                        <p class="py-6 max-w-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button class="btn btn-primary">Get Started</button>
+                    </div>
+                    <div className='h-[60vh] shrink-0'>
+                        <img src={bucketGirl} class="h-full" alt='' />
+                    </div>
+                </div>
+            </div>
+            <div className='shadow-lg p-10 w-5/6 rounded-2xl mx-auto z-20 gap-4 bg-base-200 mt-[-50px]'>
+                <h1 className='text-2xl mb-5 text-primary'>Get Free Estimate</h1>
+                <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 '>
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full  m-2" />
+                </div>
+                <button className='btn btn-primary mt-5'>Get a quote</button>
+            </div>
+        </>
+    );
+};
+
+export default Landing;
